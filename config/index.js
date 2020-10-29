12dd48606cf088a1261bbd53b59229b037cc3391
za-chengping <ping.cheng@zatech.com>
2020-10-30 00:59:16 +0800

    重新初始化提交github

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
-  projectName: 'myApp',
-  date: '2020-10-20',
+  projectName: 'studyManagers',
+  date: '2020-10-29',
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
@@ -43,7 +43,6 @@ const config = {
     }
   },
   h5: {
-    esnextModules: ['taro-ui'],
     publicPath: '/',
     staticDirectory: 'static',
     postcss: {
